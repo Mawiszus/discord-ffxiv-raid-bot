34e6d828145b8179fcf9146439142e26909007a2
Maren Awiszus <awiszus@tnt.uni-hannover.de>
2021-04-16 15:33:35 +0200

    re-added on_ready() event

--- bot.py
+++ bot.py
@@ -12,6 +12,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='$')
 
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
 @bot.command(name='hello', help='Answers with an appropriate hello message')
 async def hello(ctx):
     options = [
