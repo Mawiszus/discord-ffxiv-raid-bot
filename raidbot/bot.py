94b7cb31241594b03d489fe1f19bdca47a2f02a3
Maren Awiszus <awiszus@tnt.uni-hannover.de>
2021-05-04 18:20:59 +0200

    fix to edit-event bug try#5 (fixed user_timezone localization)

--- raidbot/bot.py
+++ raidbot/bot.py
@@ -320,7 +320,7 @@ async def edit_event(ctx, ev_id, field, value, user_timezone="UTC"):
                 try:
                     hour, minute = value.split(":")
                     dt_object = dt_object.replace(hour=int(hour), minute=int(minute), tzinfo=timezone("UTC"))
-                    dt_object = tz.normalize(tz.localize(dt_object))
+                    dt_object = tz.normalize(dt_object)
                 except Exception:
                     conn.close()
                     await ctx.send(f"Could not parse time, make sure to format like this: "
