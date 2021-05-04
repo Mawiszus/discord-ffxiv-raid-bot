73538fb217eb9361aea13c46e0af3911943aaffc
Maren Awiszus <awiszus@tnt.uni-hannover.de>
2021-05-04 18:24:59 +0200

    fix to edit-event bug try#6

--- raidbot/bot.py
+++ raidbot/bot.py
@@ -319,7 +319,7 @@ async def edit_event(ctx, ev_id, field, value, user_timezone="UTC"):
                     return
                 try:
                     hour, minute = value.split(":")
-                    dt_object = dt_object.replace(hour=int(hour), minute=int(minute), tzinfo=timezone("UTC"))
+                    dt_object = dt_object.replace(hour=int(hour), minute=int(minute), tzinfo=tz)
                     dt_object = tz.normalize(dt_object)
                 except Exception:
                     conn.close()
